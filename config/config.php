25f5bec5cc81612cd229ae29f0f84ecdc3e75cce
root <root@localhost.localdomain>
2016-02-07 15:08:32 +0000

    Initial Commit

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'localdev');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://red-dragon/avramework');
+
+
+define('PAGINATE_LIMIT', '5');
